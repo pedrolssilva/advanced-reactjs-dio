6c01a90102b645d6ac48e818641796b49fbcb156
pedro.luis <pedro.luis@flashtrader.com.br>
2022-03-01 16:18:06 -0300

    Created React app and cleaned it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import Twitter from './Twitter';
+class App extends Component {
+  state = {
+    loading: false,
+    active: true,
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({
+        loading: true})
+    },3000)
+  }
+
+  onRemove = () => {
+    const {active} = this.state
+     this.setState({
+       active: !active
+     })
+  }
+
+  render() {
+    const posts = [{
+      title: 'xpto',
+      description: 'foo'
+    },
+    {
+      title: 'xpto',
+      description: 'foo'
+    }
+  ]
+
+    return (
+      <div>
+        <button onClick={this.onRemove}> Remove component</button>
+        {this.state.active && (
+          <Twitter posts={posts} loading={this.state.loading}/>
+        )}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
