8bdf0256fbf0c1baee92ec9318ca30df915bd514
pedro.luis <pedro.luis@flashtrader.com.br>
2022-03-10 18:34:49 -0300

    Using useRef hook

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
-import PetShop from './PetShop'
-
-
+import React, {useRef, useEffect, useState} from 'react';
 
 function App () {
+  const inputRef = useRef()
+  const count = useRef(1)
+  const [, setValue] = useState(false)
+
   const handleClick = () => {
-    console.log('Shower starting...');
+    inputRef.current.focus()
+    console.log('inputRef.current:', inputRef.current)
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+    console.log('Passed here')
+      count.current = 300
+      setValue(true)
+    }, 3000)
+  })
+
   return (
-    <PetShop 
-      dogs={2}
-      customerName="PL"
-      onClick={handleClick}
-      status="done"
-    />
+    <>
+      <h1>
+        Count value: {count.current}
+      </h1>
+      Focus: <input ref={inputRef} />
+      <br />
+      <button onClick={handleClick}>To focus</button>
+    </>
   )
 }
 
